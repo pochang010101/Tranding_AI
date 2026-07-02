b1232a3be8c451e5c2e08b995f5b710ed528d2b5
chihkai <chihkai@ksi.com.tw>
2026-07-02 14:20:25 +0800

    fix: [auto] update pwa.py
    - atlas/presentation/components/pwa.py

--- atlas/presentation/components/pwa.py
+++ atlas/presentation/components/pwa.py
@@ -11,6 +11,7 @@ def inject_pwa() -> None:
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <meta name="apple-mobile-web-app-title" content="Atlas">
+    <link rel="apple-touch-icon" href="/app/static/icon-192.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <script>
       if ('serviceWorker' in navigator) {
